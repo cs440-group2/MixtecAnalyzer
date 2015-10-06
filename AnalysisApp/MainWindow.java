53053f3291050e89829f0331cb09676eb3c4961a
Hannah <Hannah@dhcp-219-15.cc.gettysburg.edu>
2015-10-06 15:49:05 -0400

    tech gui

--- AnalysisApp/MainWindow.java
+++ AnalysisApp/MainWindow.java
@@ -1,10 +1,19 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import javax.swing.AbstractListModel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
 
 public class MainWindow {
 
 	private JFrame frame;
+	private JTextField txtLemma;
 
 	/**
 	 * Launch the application.
@@ -34,8 +43,26 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 750, 600);
+		frame.setBounds(100, 100, 300, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		txtLemma = new JTextField();
+		txtLemma.setText("Lemma");
+		panel.add(txtLemma);
+		txtLemma.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		panel.add(btnSearch);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		
+		JList list = new JList();
+		panel_1.add(list);
 	}
 
 }
