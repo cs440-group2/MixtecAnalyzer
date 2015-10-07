b84605946e198bd2f288848aceed602c6008cb1a
Kyle Wigley <wiglky01@gettysburg.edu>
2015-10-06 23:05:31 -0400

    Connected RegEx search function to main applicaiton window

--- AnalysisApp/MainWindow.java
+++ AnalysisApp/MainWindow.java
@@ -4,16 +4,28 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import java.awt.BorderLayout;
 import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.awt.event.ActionEvent;
 
 public class MainWindow {
 
 	private JFrame frame;
 	private JTextField txtLemma;
+	private DefaultListModel model;
+	JList list;
 
 	/**
 	 * Launch the application.
@@ -56,13 +68,34 @@ public class MainWindow {
 		txtLemma.setColumns(10);
 		
 		JButton btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String lemma = txtLemma.getText();  //"tu3";
+
+				ArrayList<String> words = null;
+				try {
+					words = RegExp.search(lemma);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				model = new DefaultListModel();
+				for(int i = 0; i<words.size(); i++){
+					model.addElement(words.get(i));
+				}
+				list.setModel(model);
+			}
+		});
 		panel.add(btnSearch);
 		
 		JPanel panel_1 = new JPanel();
 		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
 		
-		JList list = new JList();
+		list = new JList();
 		panel_1.add(list);
+		
+		
 	}
-
+ 
 }
--- AnalysisApp/RegExp.java
+++ AnalysisApp/RegExp.java
@@ -22,6 +22,24 @@ public class RegExp {
 			System.out.println("found: " + m1.group());
 		}
 	}
+	
+	public static ArrayList<String> search(String lemma) throws FileNotFoundException {
+		
+		@SuppressWarnings("resource")
+		String file = new Scanner(new File("readme.txt")).useDelimiter("\\A").next();
+
+		char last = lemma.charAt(lemma.length()-1);
+		String derLemma = lemma.substring(0, lemma.length() - 1) + "\\(" + last + "\\)";
+		Pattern p1 = Pattern.compile("(" +lemma +"|"+derLemma+")" + "(\\=|\\b)" + "[\\w'\\(\\)]*[\\w'\\(\\)]*");
+
+		Matcher m1 = p1.matcher(file);
+
+		ArrayList<String> words = new ArrayList<String>();
+		while (m1.find()) {
+			words.add(m1.group());
+		}
+		return words;
+	}
 }
 
 
