42d1c2dac5c99212984752c1628aa6c2049ae065
Rebecca <Rebecca@dhcp-217-126.cc.gettysburg.edu>
2015-10-09 15:48:41 -0400

    search adjacent word

--- AnalysisApp/RegExp.java
+++ AnalysisApp/RegExp.java
@@ -12,13 +12,18 @@ public class RegExp {
 
 		char last = lemma.charAt(lemma.length()-1);
 		String derLemma = lemma.substring(0, lemma.length() - 1) + "\\(" + last + "\\)";
-		Pattern p1 = Pattern.compile("(" +lemma +"|"+derLemma+")" + "(\\=|\\b)" + "[\\w'\\(\\)]*[\\w'\\(\\)]*");
+		Pattern p1 = Pattern.compile("(" +lemma +"|"+derLemma+")" + "(\\=|\\b)" + "[\\w'\\(\\)]*" + "(\\=|\\b)" + "[\\w'\\(\\)]*");
+		Pattern p2 = Pattern.compile("[\\w]+" + "(\\=|\\b)" + "[\\w'\\(\\)]*" + "(\\=|\\b)" + "[\\w'\\(\\)]*");
 
 		Matcher m1 = p1.matcher(file);
+		Matcher m2 = p2.matcher(file);
 
 		List<String> words = new ArrayList<String>();
 		while (m1.find()) {
 			words.add(m1.group());
+			if (m2.find(m1.end()))
+				System.out.println("found: " + m1.group() + " " + m2.group());
+			else
 			System.out.println("found: " + m1.group());
 		}
 	}
