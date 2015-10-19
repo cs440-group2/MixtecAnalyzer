ed944998623a69365136fd69de7c97771ea8901d
Ian <Ian@138.234.215.17>
2015-10-18 22:03:25 -0400

    Dictionary parses and displays gloss

--- AnalysisApp/Dictionary.java
+++ AnalysisApp/Dictionary.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
@@ -13,12 +14,28 @@ import javax.swing.WindowConstants;
 
 public class Dictionary {
 	
-	public static void findLemmas(BufferedReader bReader) throws IOException{
+	HashMap<String, ArrayList<String>> glosses;
+	
+	
+	public void findLemmas(BufferedReader bReader) throws IOException{
 		String line = bReader.readLine();
 		ArrayList<String> lemmas = new ArrayList<String>();
+		glosses = new HashMap<String, ArrayList<String>>();
+		
+		String currentLemma = "";
+		
 		while(line != null){
 			if(line.length() >=4 && line.substring(0, 4).equals("\\lx ")){
-				lemmas.add(line.substring(4));
+				String newLemma = line.substring(4);
+				if(!newLemma.equals(currentLemma)){
+					lemmas.add(newLemma);
+					glosses.put(newLemma, new ArrayList<String>());
+					currentLemma = newLemma;
+				}
+			}
+			else if(line.length() >=7 && line.substring(0, 7).equals("\\glosa ")){
+				ArrayList<String> glossList = glosses.get(currentLemma);
+				glossList.add(line.substring(7));
 			}
 			line = bReader.readLine();
 		}
@@ -29,8 +46,21 @@ public class Dictionary {
 		
 		DefaultListModel model = new DefaultListModel();
 		
-		for(int i = 0; i<lemmas.size(); i++){
-			model.addElement(lemmas.get(i));
+		for(int l = 0; l<lemmas.size(); l++){
+			String lemma = lemmas.get(l);
+			ArrayList<String> glossList = glosses.get(lemma);
+			
+			StringBuilder sb = new StringBuilder(lemma);
+			
+			for(int g = 0; g < glossList.size(); g++){
+				sb.append(g == 0? " (" : ", ");
+				sb.append(glossList.get(g));
+				if(g==glossList.size() - 1) {sb.append(")");}
+			}
+			
+			
+			
+			model.addElement(sb.toString());
 		}
 		
 		JList list = new JList(model);
@@ -47,6 +77,10 @@ public class Dictionary {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException{
+		Dictionary dict = new Dictionary();
+	}
+	
+	public Dictionary(){
 		FileReader dictionaryReader;
 		BufferedReader bReader;
 		try {
