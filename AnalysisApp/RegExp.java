28486dae183c9f0ecf117d1047ede38e62c618e7
Rebecca <Rebecca@dhcp-223-41.cc.gettysburg.edu>
2015-10-26 09:12:39 -0400

    advanced search (following lemma)

--- AnalysisApp/RegExp.java
+++ AnalysisApp/RegExp.java
@@ -3,13 +3,19 @@ import java.util.*;
 import java.util.regex.*;
 
 public class RegExp {
-	
+
+	public static void main(String[] args) throws FileNotFoundException{
+		HashMap<String, Double> test = AdvancedSearch("tu3", 3);
+		for (String key : test.keySet())
+			System.out.println(key);
+	}
+
 	public static HashMap<String, Double> search(String lemma) throws FileNotFoundException {
-		
+
 		@SuppressWarnings("resource")
 		String file = new Scanner(new File("readme.txt")).useDelimiter("\\A").next();
 		int total = 0;
-		
+
 		char last = lemma.charAt(lemma.length()-1);
 		String derLemma = lemma.substring(0, lemma.length() - 1) + "\\(" + last + "\\)";
 		Pattern p1 = Pattern.compile("(" +lemma +"|"+derLemma+")" + "(\\=|\\b)" + "[\\w'\\(\\)]*" + "(\\=|\\b)" + "[\\w'\\(\\)]*");
@@ -34,6 +40,49 @@ public class RegExp {
 				{
 					frequency.put(origLemma,frequency.get(origLemma) + 1);
 				}
+			}
+		}
+		for (String key : frequency.keySet()) {
+			frequency.put(key,frequency.get(key)/total);
+		}
+		return frequency;
+	}
+
+	public static HashMap<String, Double> AdvancedSearch(String lemma, int NumBtwn) throws FileNotFoundException {
+
+		@SuppressWarnings("resource")
+		String file = new Scanner(new File("readme.txt")).useDelimiter("\\A").next();
+		int total = 0;
+		
+		char last = lemma.charAt(lemma.length()-1);
+		String derLemma = lemma.substring(0, lemma.length() - 1) + "\\(" + last + "\\)";
+		Pattern p1 = Pattern.compile("(" +lemma +"|"+derLemma+")" + "(\\=|\\b)" + "[\\w'\\(\\)]*" + "(\\=|\\b)" + "[\\w'\\(\\)]*");
+		Pattern p2 = Pattern.compile("[\\w]+" + "(\\=|\\b)" + "[\\w'\\(\\)]*" + "(\\=|\\b)" + "[\\w'\\(\\)]*");
+
+		Matcher m1 = p1.matcher(file);
+		Matcher m2 = p2.matcher(file);
+
+		HashMap<String, Double> frequency = new HashMap<String, Double>();
+		while (m1.find()) {
+			int count = 0;
+			m2.find(m1.start());
+			while (m2.find() && count <= NumBtwn){
+				if (count == NumBtwn){
+					total++;
+					String origLemma = m2.group().replaceAll("\\=" + "[\\w'\\(\\)]*", "");
+					if (origLemma.contains("(")){
+						origLemma = origLemma.replace("(", "");
+						origLemma = origLemma.replace(")", "");
+					}
+					if (!frequency.containsKey(origLemma)){
+						frequency.put(origLemma,1.0);
+					}
+					else
+					{
+						frequency.put(origLemma,frequency.get(origLemma) + 1);
+					}
+				}
+				count ++;
 			}
 		}
 		for (String key : frequency.keySet()) {
